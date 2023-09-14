ab60aac48d20bbea904d3f8c84ee2318ef44f632
Aspir01 <nikitovicaleks488@gmail.com>
2023-09-14 11:24:19 +0300

    deploy

---
+++ main.js
@@ -0,0 +1,25 @@
+const svg = document.querySelector('svg');
+const ul = document.querySelector('.nav-ul');
+
+svg.style.display = 'none';
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 900) {
+        svg.style.display = 'block';
+        svg.style.position = 'absolute'
+        svg.style.top = '38px'
+        svg.style.right = "-340px"
+        ul.style.display = 'none';
+    } else {
+        svg.style.display = 'none';
+        ul.style.display = 'flex';
+    }
+});
+
+svg.addEventListener('click', () => {
+    if (ul.style.display === 'block') {
+        ul.style.display = 'none';
+    } else {
+        ul.style.display = 'block';
+    }
+});
